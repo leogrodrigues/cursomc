4fbe6df400d0e11dcd5a3126f55a253bb8400f36
Leonardo Gama Rodrigues <leogrodrigues@yahoo.com.br>
2018-08-21 19:29:53 -0300

    Produto e associacao muitos para muitos

--- src/main/java/br/leonardo/cursomc/domain/Produto.java
+++ src/main/java/br/leonardo/cursomc/domain/Produto.java
@@ -8,36 +8,38 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 
 @Entity
-public class Categoria implements Serializable {
+public class Produto implements Serializable {
 
 	
 	private static final long serialVersionUID = 1L;
-	
-	
+
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	private double preco;
 	
-	@ManyToMany(mappedBy="categorias")
-	private List<Produto> produtos = new ArrayList<>();
+	@ManyToMany
+	@JoinTable(name = "PRODUTO_CATEGORIA", joinColumns = @JoinColumn(name="protudo_id"),
+		inverseJoinColumns = @JoinColumn(name="categoria_id"))
+	private List<Categoria> categorias = new ArrayList<>();
 	
-	
-	public Categoria() {
+	public Produto() {
 		
 	}
 
-	public Categoria(Integer id, String nome) {
+	public Produto(Integer id, String nome, double preco) {
 		super();
 		this.id = id;
 		this.nome = nome;
+		this.preco = preco;
 	}
-	
-	
 
 	public Integer getId() {
 		return id;
@@ -55,20 +57,28 @@ public class Categoria implements Serializable {
 		this.nome = nome;
 	}
 
-			
-	public List<Produto> getProdutos() {
-		return produtos;
+	public double getPreco() {
+		return preco;
 	}
 
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
+	public void setPreco(double preco) {
+		this.preco = preco;
 	}
 
-	//hascode para poder comparar por valores e não lugar na memoria
+		
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ((categorias == null) ? 0 : categorias.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
@@ -81,7 +91,12 @@ public class Categoria implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Produto other = (Produto) obj;
+		if (categorias == null) {
+			if (other.categorias != null)
+				return false;
+		} else if (!categorias.equals(other.categorias))
+			return false;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -95,6 +110,7 @@ public class Categoria implements Serializable {
 	
 	
 	
-
+	
+	
 	
 }
