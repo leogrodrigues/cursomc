9624687e9418c46d18f0aa034481ceca5ef908f1
Leonardo Gama Rodrigues <leogrodrigues@yahoo.com.br>
2018-08-23 15:39:16 -0300

    Endpoint /clientes/{id} disponivel

--- src/main/java/br/leonardo/cursomc/domain/Cliente.java
+++ src/main/java/br/leonardo/cursomc/domain/Cliente.java
@@ -14,6 +14,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import br.leonardo.cursomc.domain.enums.TipoCliente;
 
 @Entity
@@ -30,6 +32,7 @@ public class Cliente implements Serializable{
 //	private TipoCliente tipo; cria o construtor antes de alterar para Integer tipo
 	private Integer tipo;
 	
+	@JsonManagedReference
 	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	
