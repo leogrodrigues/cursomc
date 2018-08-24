3a79dcb9c8331fd10e54e2dad4eb6e151e254c13
Leonardo Gama Rodrigues <leogrodrigues@yahoo.com.br>
2018-08-24 16:52:32 -0300

    Atualizando Categoria com PUT

--- src/main/java/br/leonardo/cursomc/resources/CategoriaResources.java
+++ src/main/java/br/leonardo/cursomc/resources/CategoriaResources.java
@@ -25,9 +25,9 @@ public class CategoriaResources {
 	
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
 		
-		Categoria obj = service.buscar(id);
+		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 		
 		}
@@ -42,6 +42,16 @@ public class CategoriaResources {
 		return ResponseEntity.created(uri).build();
 	}
 
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id){
+		
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();		
+		
+	}
+	
+	
 	
 	
 }
