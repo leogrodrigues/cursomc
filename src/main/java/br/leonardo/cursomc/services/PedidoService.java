ce09baae62b1f249893be1390a4de5a76b7afb3d
Leonardo Gama Rodrigues <leogrodrigues@yahoo.com.br>
2018-09-05 15:35:36 -0300

    Implementando toString de Pedido

--- src/main/java/br/leonardo/cursomc/services/PedidoService.java
+++ src/main/java/br/leonardo/cursomc/services/PedidoService.java
@@ -34,6 +34,8 @@ public class PedidoService {
 	@Autowired
 	private ProdutoService produtoService;
 	
+	@Autowired
+	private ClienteService clienteService;
 	
 	
 	public Pedido find(Integer id) {
@@ -49,6 +51,7 @@ public class PedidoService {
 		
 		obj.setId(null);
 		obj.setInstance(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
 		if(obj.getPagamento() instanceof PagamentoComBoleto) {
@@ -60,10 +63,12 @@ public class PedidoService {
 		pagamentoRepository.save(obj.getPagamento());
 		for(ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
-			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
+		System.out.println(obj);
 		return obj;
 	}
 	
